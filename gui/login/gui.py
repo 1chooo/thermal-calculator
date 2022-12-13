2036389304f0d794d8aa184b57a1e50647201079
1chooo <hugo970217@gmail.com>
2022-12-14 00:51:10 +0800

    login interface

--- gui/login/gui.py
+++ gui/login/gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 
 def loginWindow():
     Login()
@@ -16,4 +17,15 @@ class Login():
 
 class initface() :
 
-    def __init__(self):
+    def __init__(self, master):
+
+        self.root = master
+        # self.root.config(bg="#98AFC7")
+        img = Image.open("../../assets/imgs/Login.jpg")
+        tk_img = ImageTk.PhotoImage(img)
+
+        canvas = tk.Canvas(self.root, width=960, height=540)
+        canvas.create_image(0, 0, anchor='nw', image=tk_img)   # 在 Canvas 中放入圖片
+        canvas.pack()
+
+        self.initface = tk.Frame(self.root)
