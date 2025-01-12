634c872e5606412c59b738c4887226db7cd41e86
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-01-12 14:48:12 +0800

    refactor: extract title and geometry settings into class attributes

--- thermal/gui/base_desk/base.py
+++ thermal/gui/base_desk/base.py
@@ -1,19 +1,18 @@
-from tkinter import *
 from thermal.gui.login.frame import LoginFrame
 
 
 def init_base(root):
-
     BaseDesk(root)
 
 
 class BaseDesk:
+    title = "Login Page"
+    geometry = "960x540"
 
     def __init__(self, master):
-
         self.root = master
         self.root.config()
-        self.root.title("Login Page")
-        self.root.geometry("960x540")
+        self.root.title(self.title)
+        self.root.geometry(self.geometry)
 
         LoginFrame(self.root)
