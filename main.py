87cfba29ba58e4fe870a4de75fab087d00e97b9f
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-01-15 10:30:11 +0800

    refactor: move main function to main.py and remove app.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 Create Date: 2023/08/19
 Author: @1chooo(Hugo ChunHo Lin)
 Version: v0.0.2
-'''
+"""
 
-from thermal.app.app import main
+from tkinter import Tk
+from thermal.gui.base_desk import base
+
+
+def main() -> None:
+    root = Tk()
+    base.init_base(root)
+    root.mainloop()
 
 if __name__ == "__main__":
-
     main()
--- thermal/app/app.py
+++
@@ -1,8 +0,0 @@
-from tkinter import Tk
-from thermal.gui.base_desk import base
-
-
-def main() -> None:
-    root = Tk()
-    base.init_base(root)
-    root.mainloop()
