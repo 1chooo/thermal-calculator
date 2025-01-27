d0d4005d5e88ca8864f2abc7f03602cd37267364
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-01-27 14:48:50 +0800

    refactor: remove unused test files and clean up dashboard class

--- thermal/gui/dashboard.py
+++ thermal/gui/dashboard.py
@@ -640,27 +640,3 @@ class Dashboard:
     def __logoutFunc(self):
         self.dashboard_frame.destroy()
         base.init_base(self.root)
-
-
-class ImageProgressing:
-    def __init__(self) -> None:
-        return
-
-    def __create_canvas(self, width_, height_) -> None:
-        self.canvas = Canvas(
-            width=width_,
-            height=height_,
-            bg="violet",
-            bd=0,
-        )
-        self.canvas.grid()
-
-    def __embed_img(self, img_path) -> None:
-        img = Image.open(img_path)
-        img = ImageTk.PhotoImage(img)
-        self.canvas.create_image(
-            0,
-            0,
-            image=img,
-            anchor="nw",
-        )
